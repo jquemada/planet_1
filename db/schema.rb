2705ba94aeb3cceaf2e4f8ba2f41defe2769802f
Juan Quemada <jquemada@dit.upm.es>
2011-11-29 07:30:17 +0100

    cambios modelo y migración site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111121171513) do
+ActiveRecord::Schema.define(:version => 20111127165324) do
+
+  create_table "sites", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "type_id"
+    t.decimal  "latitude",    :precision => 8, :scale => 6
+    t.decimal  "longitude",   :precision => 8, :scale => 6
+    t.decimal  "zoom",        :precision => 8, :scale => 6
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "types", :force => true do |t|
     t.string   "name"
