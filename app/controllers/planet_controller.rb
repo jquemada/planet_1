791152f171e8c61cab69a3c138cac078803d7fc7
carlosmagan <c.magan@alumnos.upm.es>
2011-12-06 13:21:41 +0100

    Entrega1

---
+++ app/controllers/planet_controller.rb
@@ -0,0 +1,11 @@
+class PlanetController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def author
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planet::Application.config.secret_token = 'fd9d92ebee77d942d84ecef27717cde6b44e5c8262aad4acc286eee309a75c68f1f69e06cd15d09f0716aafc33c874ad8db9f8fc11cf678a6eaea20717267064'
