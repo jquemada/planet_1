c17c3f79acb63f700d86429c7251e6969f12f43f
Léo-Paul Goffic <leo.goffic@gmail.com>
2011-12-07 16:31:58 +0100

    Added `author` page in `planet` controller
    Conflicts:
    
    	app/controllers/planet_controller.rb
    	config/routes.rb

--- app/controllers/planet_controller.rb
+++ app/controllers/planet_controller.rb
@@ -9,4 +9,20 @@ class PlanetController < ApplicationController
   def ejemplo
   end
 
+  def author
+    @authors = [
+	  # FIXME: Non-ascii characters raise an error
+	  {:name		=>	"Leo Goffic",
+	   :address		=>	"Calle de Linneo 26 Bajo B, Madrid, SPAIN",
+	   :email		=>	"leo.goffic@gmail.com",
+	   :curriculum	=>  "Erasmus student at UPM",
+	   :image 		=>	nil
+	  },
+	  {:name		=>	"Miguel Revolo",
+	   :address		=>  "Madrid, SPAIN",
+	   :email		=>  "miguel.revolo@gmail.com",
+	   :curriculum	=>	"Erasmus student at UPM"
+	  }
+	]
+  end
 end
