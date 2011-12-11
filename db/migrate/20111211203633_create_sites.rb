58574d603aa2b688eee8f164eb1f7f55d3bb2068
Victor H. Bazán <vh.bazan@upm.es>
2011-12-11 21:36:55 +0100

    creacion de scaffold site

---
+++ db/migrate/20111211203633_create_sites.rb
@@ -0,0 +1,15 @@
+class CreateSites < ActiveRecord::Migration
+  def change
+    create_table :sites do |t|
+      t.string :name
+      t.text :description
+      t.integer :type_id
+      t.decimal :latitud
+      t.decimal :longitude
+      t.decimal :zoom
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
